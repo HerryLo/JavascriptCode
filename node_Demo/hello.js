0e1af38c35e52037a60d7a49efb1f68ff8b72591
liuheng@9188.com <liuheng@9188.com>
2018-02-07 15:58:51 +0800

    '保存node_Demo'

---
+++ node_Demo/hello.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+const https = require('https');
+
+
+const cheerio = require('cheerio');
+const request = require('request');
+const url = 'https://www.pinghongbao.com/meituanwaimai/1'
+
+let req = https.get(url, (res)=> {
+    let data = '';
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+        data += chunk
+    });
+
+    res.on('end', () => {
+        const filename = path.join(__dirname, './file/hello.html');
+        const $ = cheerio.load(data);
+        fs.writeFile(filename, data, function(err){
+            if(err) console.log('写文件操作失败');
+            else console.log('写文件操作成功');
+        });
+        /*console.log(`响应主体: ${chunk}`);*/
+        console.log('------------------------------------------')
+    });
+})
+
+req.on('error', (e) => {
+  console.error(`请求遇到问题: ${e.message}`);
+});
+
+req.end();
