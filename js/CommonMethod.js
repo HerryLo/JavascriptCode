3c2dd5bade3834ad9f4fe179f557a9dadebbe632
刘恒 <13995578046@163.com>
2018-06-07 16:37:10 +0800

    添加 方法

--- js/CommonMethod.js
+++ js/CommonMethod.js
@@ -28,5 +28,47 @@ var _method = {
 		  throw new Error(('arguments Type Error'))
 	  }
 	  return str.replace(/[^\d.]/g, s);
-	}
+	},
+	/**
+	 * 时间格式转换
+	 * @param {Date}
+	 * @param {String}
+	 * @return {String}
+	 */
+	formatTime(date, str) {
+	    const _this = this;
+	    var year = date.getFullYear()
+	    var month = date.getMonth() + 1
+	    var day = date.getDate()
+
+	    var hour = date.getHours()
+	    var minute = date.getMinutes()
+	    var second = date.getSeconds()
+
+	    return [year, month, day].map(_this.formatNumber).join(str) + ' ' + [hour, minute, second].map(formatNumber).join(':')
+	},
+	/**
+	 * 加零
+	 * @param {String}
+	 * @return {String}
+	 */
+	formatNumber(n) {
+	    n = n.toString()
+	    return n[1] ? n : '0' + n
+	},
+	/**
+	 * 转换html为 text文本
+	 * @param {String}
+	 * @return {String}
+	 */
+	escapeHTML(text){
+	return text.replace(/[<>"&]/g, function(match, pos, originalText) {
+		switch (match) {
+		    case '<': return '&lt;';
+		    case '>': return '&gt;';
+		    case '&': return '&amp;';
+		    case '"': return '&quot;';
+		}
+	});
+};
 }
