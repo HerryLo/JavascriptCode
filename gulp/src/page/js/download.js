06bee020a6bc4e39ebca19c68cca16d1cffb8b0f
HerryLo <13995578046@163.com>
2018-10-30 14:28:56 +0800

    保存

--- gulp/src/page/js/download.js
+++ gulp/src/page/js/download.js
@@ -1,6 +1,6 @@
 function eleEvent() {
     const download_btn = document.getElementById('download_btn');
-    const src = 'https://myzc-open.oss-cn-hangzhou.aliyuncs.com/package/channel-duanxin/app-duanxin-release_100_jiagu_sign.apk';
+    const src = 'https://myzc-open.oss-cn-hangzhou.aliyuncs.com/package/channel-h5/app-h5-release_110_jiagu_sign.apk';
     
     download_btn.onclick = ()=> {
         location.href = src;
