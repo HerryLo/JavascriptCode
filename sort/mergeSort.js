ec1e86066733930622801edbec6f6b3ef30ab30d
HerryLo <13995578046@163.com>
2018-12-10 12:10:02 +0800

    添加合并排序

--- sort/mergeSort.js
+++ sort/mergeSort.js
@@ -1,50 +1,41 @@
 // 归并排序 
 
-let arr = [4, 56, 1, 0, 67, 99, 29]
+let result = [4, 56, 1, 0, 67, 99, 29]
 
-let len = arr.length;
-let first = 0,
-    last = len - 1;
+function mergSort(arr) {
+    let len = arr.length;
 
-function mergSort(arr, first, last) {
-    if (first < last) {
-        let mid = parseInt((last + first) / 2)
-        mergSort(arr, first, mid)
-        mergSort(arr, mid + 1, last)
-
-        console.log(first, mid+1, last)
-        mergeArray(arr, first, mid, last)
+    if(len < 2){
+        return arr;
     }
+
+    let mid = Math.floor(len / 2);
+    let left = arr.slice(0,mid);
+    let right = arr.slice(mid,len);
+    return mergeArray(mergSort(left), mergSort(right));//递归分别对左右两部分数组进行排序合并
 }
 
-function mergeArray(a, first, mid, last) {
-    let l = first;
-    let r = last;
-    let m = mid;
-    let temp = [];
+function mergeArray(left, right) {
+    let arr = [];
 
-    for (let i = l; i <= r; i++) {
-        temp[i - l] = a[i];
-    }
-
-    let i = l;
-    let j = m + 1;
-    for (let k = l; k <= r; k++) {
-        // console.log(i, m, j, r)
-        if (i > m) {
-            a[k] = temp[j - l];
-            j++;
-        } else if (j > r) {
-            a[k] = temp[i - l];
-            i++;
-        } else if (temp[i - l] > temp[j - l]) {
-            a[k] = temp[j - l];
-            j++;
-        } else if (temp[i - l] < temp[j - l]) {
-            a[k] = temp[i - l];
-            i++;
+    while(left.length > 0 && right.length > 0){
+        if(left[0]<=right[0]){
+            //如果左边的数据小于右边的数据，将左边数据取出，放在新数组中
+            arr.push(left.shift());
+        }else{
+            arr.push(right.shift());
         }
     }
+        
+    while(left.length){
+        arr.push(left.shift())
+    }
+
+    while(right.length){
+        arr.push(right.shift())
+    }
+
+    return arr;
 }
 
-mergSort(arr, first, last)
+console.log(mergSort(result))
